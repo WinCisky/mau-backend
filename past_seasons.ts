53a861eeb8c6a082ff1c219b609cff0f286139ed
Simone <xsimone97@hotmail.it>
2024-01-19 17:33:34 +0100

    fix pas season completed

--- past_seasons.ts
+++ past_seasons.ts
@@ -3,14 +3,27 @@ import type PocketBase from "https://esm.sh/pocketbase@0.15.3";
 import { fillAnime } from "./helper_anime.ts";
 const MY_URL = "https://www.animeunity.tv";
 
-function getAnimeWithoutPastSeasons(pb: PocketBase) {
-    return pb.collection('mau_anime').getFirstListItem(`past_seasons_checked=false`);
+async function getAnimeWithoutPastSeasons(pb: PocketBase) {
+    let anime = null;
+    try {
+        anime = await pb.collection('mau_anime').getFirstListItem(`past_seasons_checked=false`);
+    } catch (_err) {
+        console.log("no anime without past seasons");
+    }
+
+    return anime;
 }
 
 // meantto fill past seasons
 export async function pastSeasons(pb: PocketBase) {
     // get anime to check (past_seasons_checked = false)
     const anime = await getAnimeWithoutPastSeasons(pb);
+    if (!anime) {
+        console.log("Done!");
+        return;
+    } else {
+        console.log(`filling past seasons for: ${anime.mau_id}-${anime.slug}`);
+    }
     const animeMauId = anime.mau_id;
     const animeSlug = anime.slug;
     const animeUrl = `${MY_URL}/anime/${animeMauId}-${animeSlug}`;
