99790690c5d52963752ba9408a51297f8176cb8d
Simone <xsimone97@hotmail.it>
2024-03-30 19:47:19 +0100

    update missing info

--- fill_missing_details.ts
+++ fill_missing_details.ts
@@ -1,27 +1,34 @@
 import PocketBase from "https://esm.sh/pocketbase@0.15.3";
+import { getAnimeDetailsMalApi } from "./helper_anime.ts";
 
 export async function fillMissingDetails(pb: PocketBase) {
-    const anime = await pb.collection('mau_anime').getList(1, 200, {
-        filter: 'video = null'
+    const anime = await pb.collection('mau_anime').getList(1, 1000, {
+        filter: 'nsfw = null'
     });
     for (const item of anime.items) {
         const mal_id = item.mal_id;
-        // https://api.jikan.moe/v4/anime/${id}/videos
-        const resp = await fetch(`https://api.jikan.moe/v4/anime/${mal_id}/full`);
-        const mal = await resp.json();
-        const video_id = mal?.data?.trailer?.youtube_id ?? "-";
-        // set video of anime
-        const animeData = {
-            "video": video_id
+        const malApiDetails = await getAnimeDetailsMalApi(mal_id, ["nsfw", "rating"]);
+        if (malApiDetails) {
+            const animeData = {
+                "nsfw": null,
+                "rating": null
+            };
+            if (malApiDetails.nsfw) {
+                animeData["nsfw"] = malApiDetails.nsfw;
+            }
+            if (malApiDetails.rating) {
+                animeData["rating"] = malApiDetails.rating;
+            }
+            try {
+                await pb.collection('mau_anime').update(item.id, animeData);
+                console.log(`updated nsfw and rating: ${mal_id}`);
+            } catch (_err){
+                console.log(`error updating nsfw and rating: ${mal_id}`);
+            }
         }
-        try {
-            await pb.collection('mau_anime').update(item.id, animeData);
-            console.log(`updated video: ${mal_id}`);
-        } catch (_err){
-            console.log(`error updating video: ${mal_id}`);
-        }
-        // wait 1 second
-        await new Promise(resolve => setTimeout(resolve, 1000));
+        // wait 3-30s randomly
+        const waitTime = Math.floor(Math.random() * 30) + 3;
+        await new Promise(resolve => setTimeout(resolve, waitTime * 1000));
     }
     // console.log(anime);
     console.log("Done!");
