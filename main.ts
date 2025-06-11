258a4199d3361b831991bf48e53f0fdb634b0027
Simone <xsimone97@hotmail.it>
2025-06-11 21:12:15 +0000

    kv video cache

--- main.ts
+++ main.ts
@@ -26,14 +26,29 @@ const router = new Router();
 router
   // get video url
   .get("/api/mirror/:id", async (context) => {
-    if (context?.params?.id) {
-      const range = context.request.headers.get("range");
-      if (!range) {
-        context.response.status = 416; // Range Not Satisfiable
-        context.response.body = "Range header is required";
-        return;
-      }
+    const videoId = context?.params?.id ? parseInt(context?.params?.id) : null;
+    if (!videoId || isNaN(videoId)) {
+      context.response.status = 400; // Bad Request
+      context.response.body = "Invalid video ID";
+      return;
+    }
+    
+    const range = context.request.headers.get("range");
+    if (!range) {
+      context.response.status = 416; // Range Not Satisfiable
+      context.response.body = "Range header is required";
+      return;
+    }
 
+    let videoUrl: string | undefined;
+
+    const kv = await Deno.openKv();
+    const cachedUrl = await kv.get<string>(`video:${videoId}`);
+    if (cachedUrl) {
+      console.log(`Cache hit for video ID: ${videoId}`);
+      videoUrl = cachedUrl.value;
+    } else {
+      console.log(`Cache miss for video ID: ${videoId}`);
       const result = await getVideoUrl(
         parseInt(context?.params?.id),
         context?.request.ip,
@@ -44,34 +59,45 @@ router
         return;
       }
 
-      const videoRequest = await fetch(result, {
-        headers: {
-          "Range": range,
-        },
-      });
+      videoUrl = result;
+      // cache for 30 minutes
+      const expireIn = 60 * 60 * 1000; // 60 minutes in milliseconds
+      await kv.set(`video:${videoId}`, videoUrl, { expireIn });
+    }
 
-      if (videoRequest.ok) {
-        const readableStream = videoRequest.body;
-        if (!readableStream) {
-          context.response.status = 404;
-          context.response.body = "Video not found";
-          return;
-        }
+    if (!videoUrl) {
+      context.response.status = 404; // Not Found
+      context.response.body = "Video URL not found";
+      return;
+    }
 
-        const contentRange = videoRequest.headers.get("content-range");
-        const contentLength = videoRequest.headers.get("content-length");
+    const videoRequest = await fetch(videoUrl, {
+      headers: {
+        "Range": range,
+      },
+    });
 
-        context.response.status = 206; // Partial Content
-        context.response.headers.set("content-type", "video/mp4");
-        context.response.headers.set("content-range", contentRange || "");
-        context.response.headers.set("content-length", contentLength || "");
-        context.response.headers.set("accept-ranges", "bytes");
-
-        context.response.body = readableStream;
-      } else {
-        context.response.status = 500;
-        context.response.body = "Error fetching video";
+    if (videoRequest.ok) {
+      const readableStream = videoRequest.body;
+      if (!readableStream) {
+        context.response.status = 404;
+        context.response.body = "Video not found";
+        return;
       }
+
+      const contentRange = videoRequest.headers.get("content-range");
+      const contentLength = videoRequest.headers.get("content-length");
+
+      context.response.status = 206; // Partial Content
+      context.response.headers.set("content-type", "video/mp4");
+      context.response.headers.set("content-range", contentRange || "");
+      context.response.headers.set("content-length", contentLength || "");
+      context.response.headers.set("accept-ranges", "bytes");
+
+      context.response.body = readableStream;
+    } else {
+      context.response.status = 500;
+      context.response.body = "Error fetching video";
     }
   })
   // get latest episodes
